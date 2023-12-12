79a4109ae62d5f15986e14c24658f06e951e9e87
hg-ms <53219833+hg-ms@users.noreply.github.com>
2023-12-12 07:09:33 +0100

    license text generated

--- communication/binary/src/main/java/module-info.java
+++ communication/binary/src/main/java/module-info.java
@@ -1,3 +1,16 @@
+/*-
+ * #%L
+ * Eclipse Serializer Communication Binary
+ * %%
+ * Copyright (C) 2023 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
 module org.eclipse.serializer.communication.binary
 {
 	exports org.eclipse.serializer.communication.binary.types;
--- communication/binary/src/main/java/org/eclipse/serializer/communication/binarydynamic/ComMessageNewType.java
+++ communication/binary/src/main/java/org/eclipse/serializer/communication/binarydynamic/ComMessageNewType.java
@@ -1,5 +1,19 @@
 package org.eclipse.serializer.communication.binarydynamic;
 
+/*-
+ * #%L
+ * Eclipse Serializer Communication Binary
+ * %%
+ * Copyright (C) 2023 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import org.eclipse.serializer.chars.VarString;
 import org.eclipse.serializer.persistence.types.PersistenceTypeDefinition;
 import org.eclipse.serializer.persistence.types.PersistenceTypeDictionaryAssembler;
