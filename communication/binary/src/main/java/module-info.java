8a04910cbebe94b54ce721f0ad6a6f29599dd4bb
Zdenek Jonas <z.jonas@microstream.one>
2024-02-15 13:43:34 +0100

    java module-info back  (#101)
    add module-info.java files

---
+++ communication/binary/src/main/java/module-info.java
@@ -0,0 +1,22 @@
+/*-
+ * #%L
+ * microstream-communication-binary
+ * %%
+ * Copyright (C) 2019 - 2022 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+module org.eclipse.serializer.communication.binary
+{
+	exports org.eclipse.serializer.communication.binary.types;
+	exports org.eclipse.serializer.communication.binarydynamic;
+	exports org.eclipse.serializer.communication.tls;
+	
+	requires transitive org.eclipse.serializer.communication;
+	requires transitive org.eclipse.serializer.persistence.binary;
+}
