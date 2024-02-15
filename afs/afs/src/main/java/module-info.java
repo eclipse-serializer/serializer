02ed71cc41f2261512fd06e4d8fd9a1da8ae1463
Zdenek Jonas <z.jonas@microstream.one>
2024-02-15 13:43:34 +0100

    java module-info back  (#101)
    add module-info.java files
    
    (cherry picked from commit 8a04910cbebe94b54ce721f0ad6a6f29599dd4bb)

---
+++ afs/afs/src/main/java/module-info.java
@@ -0,0 +1,21 @@
+/*-
+ * #%L
+ * Eclipse Serializer Abstract File System
+ * %%
+ * Copyright (C) 2023 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+module org.eclipse.serializer.afs
+{
+	/* All packages are exported */
+	exports org.eclipse.serializer.afs.exceptions;
+	exports org.eclipse.serializer.afs.types;
+
+	requires transitive org.eclipse.serializer.base;
+}
