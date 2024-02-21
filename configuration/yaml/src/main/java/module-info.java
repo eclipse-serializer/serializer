a0fae534fdaf2e422c4860876305fb66ad7a64ef
Zdenek Jonas <z.jonas@microstream.one>
2024-02-21 13:45:19 +0100

    Zj/rel 1 2 0 prepare (#105)
    * java module-info back  (#101)
    
    add module-info.java files
    
    (cherry picked from commit 8a04910cbebe94b54ce721f0ad6a6f29599dd4bb)
    
    * Fix DBB cleaner invocation (#103)
    
    (cherry picked from commit 805c42adb59eaa4ea8025b5326cd0721f3332a08)
    
    * release dev version
    
    ---------
    
    Co-authored-by: Florian Habermann <f.habermann@microstream.one>

---
+++ configuration/yaml/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+/*-
+ * #%L
+ * Eclipse Serializer Configuration YAML
+ * %%
+ * Copyright (C) 2023 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+module org.eclipse.serializer.configuration.yaml
+{
+	exports org.eclipse.serializer.configuration.yaml.types;
+	
+	requires transitive org.eclipse.serializer.configuration;
+	requires transitive org.yaml.snakeyaml;
+}
