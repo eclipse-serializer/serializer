382a5359ce48a382d2c244c70c44aa2fca1bc38b
hg-ms <53219833+hg-ms@users.noreply.github.com>
2024-12-05 10:45:03 +0100

    doc

--- persistence/persistence/src/main/java/org/eclipse/serializer/persistence/types/PersistenceObjectCollector.java
+++ persistence/persistence/src/main/java/org/eclipse/serializer/persistence/types/PersistenceObjectCollector.java
@@ -3,17 +3,21 @@ package org.eclipse.serializer.persistence.types;
 /**
  * Define an interface to collect all objects and their IDs that get registered
  * to be persisted by a storer.
- * 
+ * <br>
  * The interface define only one method that accepts the object and its ID.
- * 
+ * <br>
  * Any other logic like clearing between stores has to be done by the implementer.
  * 
  */
 public interface PersistenceObjectCollector
 {
 	/**
-	 * 
-	 * 
+	 * Can be called by {@link Storer} implementations when an object
+	 * gets registered to be stored.
+	 * <br>
+	 * As this method may get invoked several times during one store
+	 * implementers should minimize any logic here.
+	 *
 	 * @param objectID the object ID
 	 * @param object the object instance
 	 */
