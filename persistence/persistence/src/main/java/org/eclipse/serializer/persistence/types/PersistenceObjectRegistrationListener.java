351b89664724b3f0951632e6f772a7bf3c3748b5
fh-ms <f.habermann@microstream.one>
2024-12-11 16:16:11 +0100

    Add missing license header

--- persistence/persistence/src/main/java/org/eclipse/serializer/persistence/types/PersistenceObjectRegistrationListener.java
+++ persistence/persistence/src/main/java/org/eclipse/serializer/persistence/types/PersistenceObjectRegistrationListener.java
@@ -1,5 +1,19 @@
 package org.eclipse.serializer.persistence.types;
 
+/*-
+ * #%L
+ * Eclipse Serializer Persistence
+ * %%
+ * Copyright (C) 2023 - 2024 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 /**
  * Define an interface to collect all objects and their IDs that get registered
  * to be persisted by a storer.
