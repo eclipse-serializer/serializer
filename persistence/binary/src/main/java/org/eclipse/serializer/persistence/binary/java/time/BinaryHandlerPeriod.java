dde72127e45ece523de95d450b00744133d801ad
hg-ms <53219833+hg-ms@users.noreply.github.com>
2025-06-18 07:03:33 +0200

    replaced by nearly identical android handler...

--- persistence/binary/src/main/java/org/eclipse/serializer/persistence/binary/java/time/BinaryHandlerPeriod.java
+++ persistence/binary/src/main/java/org/eclipse/serializer/persistence/binary/java/time/BinaryHandlerPeriod.java
@@ -1,5 +1,19 @@
 package org.eclipse.serializer.persistence.binary.java.time;
 
+/*-
+ * #%L
+ * Eclipse Serializer Persistence Android
+ * %%
+ * Copyright (C) 2023 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import java.time.Period;
 
 import org.eclipse.serializer.persistence.binary.types.AbstractBinaryHandlerCustomNonReferentialFixedLength;
@@ -7,8 +21,13 @@ import org.eclipse.serializer.persistence.binary.types.Binary;
 import org.eclipse.serializer.persistence.types.PersistenceLoadHandler;
 import org.eclipse.serializer.persistence.types.PersistenceStoreHandler;
 
-public class BinaryHandlerPeriod extends AbstractBinaryHandlerCustomNonReferentialFixedLength<Period>
+public final class BinaryHandlerPeriod extends AbstractBinaryHandlerCustomNonReferentialFixedLength<Period>
 {
+	static final long BINARY_OFFSET_YEARS  =                                   0L;
+	static final long BINARY_OFFSET_MONTHS = BINARY_OFFSET_YEARS  + Integer.BYTES;
+	static final long BINARY_OFFSET_DAYS   = BINARY_OFFSET_MONTHS + Integer.BYTES;
+	static final long BINARY_LENGTH        = BINARY_OFFSET_DAYS   + Integer.BYTES;
+	
 	///////////////////////////////////////////////////////////////////////////
 	// static methods //
 	///////////////////
@@ -17,48 +36,61 @@ public class BinaryHandlerPeriod extends AbstractBinaryHandlerCustomNonReferenti
 	{
 		return new BinaryHandlerPeriod();
 	}
+
 	
 	
 	///////////////////////////////////////////////////////////////////////////
 	// constructors //
 	/////////////////
-	
+
 	BinaryHandlerPeriod()
 	{
 		super(
 			Period.class,
 			CustomFields(
-				CustomField(int.class, "years"),
+				CustomField(int.class, "years" ),
 				CustomField(int.class, "months"),
-				CustomField(int.class, "days")
+				CustomField(int.class, "days"  )
 			)
 		);
 	}
 
 
-	@Override
-	public void updateState(Binary data, Period instance, PersistenceLoadHandler handler)
-	{
-		//no-op
-	}
+
+	///////////////////////////////////////////////////////////////////////////
+	// methods //
+	////////////
+	
 
 	@Override
-	public void store(Binary data, Period instance, long objectId, PersistenceStoreHandler<Binary> handler)
+	public final void store(
+		final Binary                          data    ,
+		final Period                          instance,
+		final long                            objectId,
+		final PersistenceStoreHandler<Binary> handler
+	)
 	{
-		data.storeEntityHeader(Integer.BYTES * 3, this.typeId(), objectId);
-		data.store_int(0, instance.getYears());
-		data.store_int(Integer.BYTES, instance.getMonths());
-		data.store_int(Integer.BYTES * 2, instance.getDays());
-	}
-
-	@Override
-	public Period create(Binary data, PersistenceLoadHandler handler)
-	{
-		int years = data.read_int(0);
-		int months = data.read_int(Integer.BYTES);
-		int days = data.read_int(Integer.BYTES * 2);
+		data.storeEntityHeader(BINARY_LENGTH, this.typeId(), objectId);
 		
-		return Period.of(years, months, days);
+		data.store_int(BINARY_OFFSET_YEARS , instance.getYears());
+		data.store_int(BINARY_OFFSET_MONTHS, instance.getMonths());
+		data.store_int(BINARY_OFFSET_DAYS  , instance.getDays());
+	}
+
+	@Override
+	public final Period create(final Binary data, final PersistenceLoadHandler handler)
+	{
+		return Period.of(
+			data.read_int(BINARY_OFFSET_YEARS),
+			data.read_int(BINARY_OFFSET_MONTHS),
+			data.read_int(BINARY_OFFSET_DAYS)
+		);
+	}
+
+	@Override
+	public final void updateState(final Binary data, final Period instance, final PersistenceLoadHandler handler)
+	{
+		// no-op
 	}
 
 }
