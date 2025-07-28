c56b94e17f67fe26c477764f20bf1e2bf00e72c0
Zdenek Jonas <z.jonas@microstream.one>
2025-07-28 15:00:30 +0200

    Revert "test - remove licence header"
    This reverts commit 216a12fadeaca522109787a0ed3ce4ad277de70b.

--- afs/afs/src/main/java/org/eclipse/serializer/afs/exceptions/AfsException.java
+++ afs/afs/src/main/java/org/eclipse/serializer/afs/exceptions/AfsException.java
@@ -1,6 +1,18 @@
 package org.eclipse.serializer.afs.exceptions;
 
-
+/*-
+ * #%L
+ * Eclipse Serializer Abstract File System
+ * %%
+ * Copyright (C) 2023 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
 
 import org.eclipse.serializer.exceptions.BaseException;
 
