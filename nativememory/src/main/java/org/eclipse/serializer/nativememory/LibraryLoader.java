9c48f576d07960fcb56d390dd6a390f453e9d8a2
hg-ms <53219833+hg-ms@users.noreply.github.com>
2025-11-27 13:22:00 +0100

    drafts

--- nativememory/src/main/java/org/eclipse/serializer/nativememory/LibraryLoader.java
+++ nativememory/src/main/java/org/eclipse/serializer/nativememory/LibraryLoader.java
@@ -28,43 +28,60 @@ public class LibraryLoader
 {
 	private final static Logger logger = Logging.getLogger(NativeMemoryAccessor.class);
 	
+	private final static String JAR_NATIVEFOLDER = "native";
+	private final static String LIBRARY_BASE_NAME = "libEclipseStoreNativeMemory";
+	
 	private final static String PROPERTY_OS_NAME = System.getProperty("os.name").toLowerCase();
 	private final static String PROPERTY_OS_ARCH = System.getProperty("os.arch").toLowerCase();
 	
 	private final static String OS_NAME;
-	private final static String AS_ARCH;
+	private final static String OS_ARCH;
 	private final static String RELEASE_VERSION;
+	private final static String LIB_FILE_EXTENSION;
 	
 	static {
 		OS_NAME = switch(PROPERTY_OS_NAME) {
-			case String os_name when os_name.startsWith("windows") -> "WIN";
-			case String os_name when os_name.startsWith("linux") -> "LINUX";
-			case String os_name when os_name.startsWith("mac") -> "MACOS";
-			case String os_name when os_name.startsWith("Darwin") -> "MACOS";
+			case String os_name when os_name.startsWith("windows") -> "windows";
+			case String os_name when os_name.startsWith("linux") -> "ubuntu";
+			case String os_name when os_name.startsWith("mac") -> "macos";
+			case String os_name when os_name.startsWith("Darwin") -> "macos";
 			default -> "UNKOWN";
 		};
 		
-		AS_ARCH = switch(PROPERTY_OS_ARCH) {
-			case String os_name when os_name.contains("64") -> "64";
+		OS_ARCH = switch(PROPERTY_OS_ARCH) {
+			case String os_name when os_name.contains("aarch64") -> "arm";
+			case String os_name when os_name.contains("arm64") -> "arm";
+			case String os_name when os_name.contains("amd64") -> "x86_amd64";
 			default -> "UNKOWN";
 		};
+		
+		LIB_FILE_EXTENSION = switch(PROPERTY_OS_NAME) {
+			case String os_name when os_name.startsWith("windows") -> ".dll";
+			case String os_name when os_name.startsWith("linux") -> ".so";
+			case String os_name when os_name.startsWith("mac") -> ".dylib";
+			case String os_name when os_name.startsWith("Darwin") ->".dylib";
+		default -> "UNKOWN";
+	};
 		
 		RELEASE_VERSION = getReleaseVersion();
 		
+		String libPath = buildInternalPath(buildLibraryName());
+		logger.info("selected native library: {}", libPath);
 		
-		String library = extractLibrary("natives/windows/libEclipseStoreNativeMemory-windows-x86_64.dll");
+		//String library = extractLibrary("natives/windows/libEclipseStoreNativeMemory-windows-x86_64.dll");
 		
-		System.load(library);
+//		String library = extractLibrary(buildInternalPath(buildLibraryName()));
+//		System.load(library);
 		
 	}
 		
 	public LibraryLoader() {
 		logger.info("OS Name: {},  arch: {}", PROPERTY_OS_NAME, PROPERTY_OS_ARCH);
 		
-		logger.info("OS Name: {},  arch: {}, version {}", OS_NAME, AS_ARCH, RELEASE_VERSION);
+		logger.info("OS Name: {},  arch: {}, version {}", OS_NAME, OS_ARCH, RELEASE_VERSION);
 		
 	}
-			
+	
 	private static String getReleaseVersion() {
 		
 		final String implVersion = "Implementation-Version";
@@ -80,6 +97,14 @@ public class LibraryLoader
 		} catch(IOException e) {
 			throw new RuntimeException("Failed to read implementation version from manifest!");
 		}
+	}
+	
+	private static String buildLibraryName() {
+		return LIBRARY_BASE_NAME + "-" + OS_NAME + "-" + OS_ARCH + LIB_FILE_EXTENSION;
+	}
+	
+	private static String buildInternalPath(String libName) {
+		return JAR_NATIVEFOLDER + "/" + OS_NAME + "/" + libName;
 	}
 	
 	private static String extractLibrary(String name) {
