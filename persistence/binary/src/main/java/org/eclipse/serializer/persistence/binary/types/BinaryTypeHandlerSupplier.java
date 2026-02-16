03b224b49e122c8653d2664ea59a4cf655b71887
hg-ms <53219833+hg-ms@users.noreply.github.com>
2026-02-16 14:08:07 +0100

    Supporting gigmap handler initialization for LegacyTypeHandler

---
+++ persistence/binary/src/main/java/org/eclipse/serializer/persistence/binary/types/BinaryTypeHandlerSupplier.java
@@ -0,0 +1,22 @@
+package org.eclipse.serializer.persistence.binary.types;
+
+/*-
+ * #%L
+ * Eclipse Serializer Persistence Binary
+ * %%
+ * Copyright (C) 2023 - 2026 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
+import org.eclipse.serializer.persistence.types.PersistenceLegacyTypeHandlerSupplier;
+
+public interface BinaryTypeHandlerSupplier<T> extends PersistenceLegacyTypeHandlerSupplier<Binary, T>
+{
+
+}
---
+++ persistence/persistence/src/main/java/org/eclipse/serializer/persistence/types/PersistenceLegacyTypeHandlerSupplier.java
@@ -0,0 +1,26 @@
+package org.eclipse.serializer.persistence.types;
+
+/*-
+ * #%L
+ * Eclipse Serializer Persistence
+ * %%
+ * Copyright (C) 2023 - 2026 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
+/**
+ * Classes implementing this interface must supply a PersistenceLegacyTypeHandler
+ * 
+ * @param <D> Persitence implementation type
+ * @param <T> Class<?> of the legacy type
+ */
+public interface PersistenceLegacyTypeHandlerSupplier<D,T>
+{
+	PersistenceLegacyTypeHandler<D,T> getLegacyTypeHandler();
+}
