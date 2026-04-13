9f0b90bb044c30167baa9cd1e721a58373172288
fh-ms <f.habermann@microstream.one>
2026-04-13 15:59:35 +0200

    Add license header

--- base/src/main/java/org/eclipse/serializer/collections/OffHeapLongHashSet.java
+++ base/src/main/java/org/eclipse/serializer/collections/OffHeapLongHashSet.java
@@ -1,5 +1,19 @@
 package org.eclipse.serializer.collections;
 
+/*-
+ * #%L
+ * Eclipse Serializer Base
+ * %%
+ * Copyright (C) 2026 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import org.eclipse.serializer.math.XMath;
 import org.eclipse.serializer.memory.XMemory;
 
